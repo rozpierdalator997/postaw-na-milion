294d63e6209b53f6c29e754baeed53028c98fa97
rozpierdalator997 <szymonm715@gmail.com>
2026-07-23 22:01:08 +0200

    Update dane.js

--- dane.js
+++ dane.js
@@ -11,7 +11,7 @@ latwe: [
     { kategoria: "Historia", pytanie: "Czemu ty się kurwa wylałeś na ścianę do chuja wafla?", odpowiedzi: ["Bo tak", "eureeurru", "Bo ja", "Bo nie"], poprawna: 2, media: "film1" },
     { kategoria: "Sztuka", pytanie: "Co przedstawia ten obraz?", odpowiedzi: ["Żyd ze szlugiem", "Żyd po litrze czystej", "Żydowskie szczęście", "Żydowski kolczyk"], poprawna: 3, media: "zdj3", mediaOdkryte: "zdj3odkryte" },
     { kategoria: "Sport", pytanie: "Ile zawodników jednej drużyny gra na boisku w piłce nożnej?", odpowiedzi: ["9", "10", "11", "12"], poprawna: 2, media: "zdj1" },
-    { kategoria: "Film", pytanie: "Kto reżyserował 'Incepcję'?", odpowiedzi: ["Steven Spielberg", "Christopher Nolan", "James Cameron", "Ridley Scott"], poprawna: 1, media: "zdj2" },
+    { kategoria: "Kinematografia", pytanie: "Co się stało z Panem Witczakiem?", odpowiedzi: ["Kurrrbleeueue", "Nic", "Jest dobrze w chuj", "Zesrał się"], poprawna: 0, media: "film2" },
     { kategoria: "Muzyka", pytanie: "Ile strun ma standardowa gitara?", odpowiedzi: ["4", "5", "6", "7"], poprawna: 2, media: "zdj2" },
     { kategoria: "Nauka", pytanie: "Jaki jest chemiczny symbol złota?", odpowiedzi: ["Go", "Gd", "Au", "Ag"], poprawna: 2, media: "zdj1" },
     { kategoria: "Kuchnia", pytanie: "Z jakiego kraju pochodzi pizza?", odpowiedzi: ["Francja", "Hiszpania", "Włochy", "Grecja"], poprawna: 2, media: "zdj2" },

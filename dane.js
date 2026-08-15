0682254ed7ad4883e07ae8aaa97811d4d5186384
rozpierdalator997 <szymonm715@gmail.com>
2026-08-15 23:08:56 +0200

    Add new quiz questions to gameData

--- dane.js
+++ dane.js
@@ -10,15 +10,15 @@ const gameData = {
 
 zestaw1: {
     latwe: [
-    { kategoria: "Historia", pytanie: "Czemu ty się kurwa wylałeś na ścianę do chuja wafla?", odpowiedzi: ["Bo tak", "eureeurru", "Bo ja", "Bo nie"], poprawna: 2, media: "film1" },
-    { kategoria: "Sztuka", pytanie: "Co przedstawia ten obraz?", odpowiedzi: ["Żyd ze szlugiem", "Żyd po litrze czystej", "Żydowskie szczęście", "Żydowski kolczyk"], poprawna: 3, media: "zdj3", mediaOdkryte: "zdj3odkryte" },
-    { kategoria: "Sport", pytanie: "Ile zawodników jednej drużyny gra na boisku w piłce nożnej?", odpowiedzi: ["9", "10", "11", "12"], poprawna: 2, media: "zdj1" },
-    { kategoria: "Kinematografia", pytanie: "Co się stało z Panem Witczakiem?", odpowiedzi: ["Kurrrbleeueue", "Nic", "Jest dobrze w chuj", "Zesrał się"], poprawna: 0, media: "film2" },
-    { kategoria: "Muzyka", pytanie: "Jaki jest tytuł tego utworu?", odpowiedzi: ["Mickey Rourke", "Fame", "II Duce", "Zabóstwo Liryczne"], poprawna: 0, media: "Mickey Rourke" },
-    { kategoria: "Nauka", pytanie: "Jaki jest chemiczny symbol złota?", odpowiedzi: ["Go", "Gd", "Au", "Ag"], poprawna: 2, media: "zdj1" },
-    { kategoria: "Kuchnia", pytanie: "Z jakiego kraju pochodzi pizza?", odpowiedzi: ["Francja", "Hiszpania", "Włochy", "Grecja"], poprawna: 2, media: "zdj2" },
-    { kategoria: "Zwierzęta", pytanie: "Jak nazywa się największe zwierzę na świecie?", odpowiedzi: ["Słoń afrykański", "Płetwal błękitny", "Żyrafa", "Rekin wielorybi"], poprawna: 1, media: "zdj1" },
-    { kategoria: "Technologia", pytanie: "Jaka firma stworzyła iPhone'a?", odpowiedzi: ["Samsung", "Apple", "Google", "Microsoft"], poprawna: 1, media: "zdj2" },
+    { kategoria: "Geografia", pytanie: "Jakiego kraju są te kontury?", odpowiedzi: ["Syria", "Polska", "Łotwa", "Uganda"], poprawna: 1, media: "l114" },
+    { kategoria: "Piłka nożna", pytanie: "Co to za piłkarz?", odpowiedzi: ["To nie piłkarz tylko jakiś murzyn", "Ousmane Dembele", "Eusébio da Silva Ferreira", "Franco Baresi"], poprawna: 1, media: "l103" },
+    { kategoria: "Historia", pytanie: "W którym roku był chrzest Polski?", odpowiedzi: ["966", "996", "1410", "1000"], poprawna: 0, media: "l108" },
+    { kategoria: "Filmy animowane", pytanie: "Tygrysica, Małpa, Żmija, Modliszka, jakie zwierzę jeszcze było w Wielkiej Piątce?", odpowiedzi: ["Czapla", "Żuraw", "Bocian", "Paw"], poprawna: 1, media: "l127" },
+    { kategoria: "Znane powiedzenia", pytanie: "Kto wypowiedział słynne słowa Veni, vidi, vici?", odpowiedzi: ["Oktawian August", "Neron", "Marek Aureliusz", " Juliusz Cezar"], poprawna: 3, media: "l111" },
+    { kategoria: "USA", pytanie: "Jaka jest stolica Stanów Zjednoczonych?", odpowiedzi: ["Waszyngton", "Nowy Jork", "Los Angeles", "Houston"], poprawna: 0, media: "l119" },
+    { kategoria: "Artyści", pytanie: "Jak się nazywa ten jegomość?", odpowiedzi: ["Borys Przybylski", "Piotr Połać", "Winicjusz Bartków", "Robert Kazanowski"], poprawna: 1, media: "l105" },
+    { kategoria: "Siłownia", pytanie: "Które mięśnie nie pracują podczas robienia poprawnych pompek?", odpowiedzi: ["Triceps", "Klatka piersiowa", "Brzuch", "Żadna odpowiedź nie jest poprawna"], poprawna: 3, media: "l130" },
+    { kategoria: "Europa", pytanie: "Ilu mieszkańców ma Europa (kontynent)?", odpowiedzi: ["750 mln", "1 mld", "450 mln", "1,5 mld"], poprawna: 0, media: "l101" },
     { kategoria: "Literatura", pytanie: "Kto napisał 'Pana Tadeusza'?", odpowiedzi: ["Juliusz Słowacki", "Adam Mickiewicz", "Henryk Sienkiewicz", "Cyprian Kamil Norwid"], poprawna: 1, media: "zdj1" }
   ],
 
